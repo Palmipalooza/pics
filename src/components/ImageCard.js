26ff67aa44196c5e000dc4dee6710324bd931378
Palmi Moller <palmimoller@me.com>
2020-06-01 14:12:31 -0600

    Added css grid spacing for each image based on it's size for better tiling

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,19 +4,30 @@ class ImageCard extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      spans: 0,
+    };
+
     this.imageRef = createRef();
   }
 
-  async componentDidMount() {
-    const image = await this.imageRef.current.clientHeight;
-    console.log(image);
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
   }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans });
+  };
 
   render() {
     const { urls, description } = this.props.image;
 
     return (
-      <div>
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
         <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
