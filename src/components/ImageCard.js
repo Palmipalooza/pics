20194579323e861ac9b60092bd4c2592b1fdb96e
Palmi Moller <palmimoller@me.com>
2020-06-01 10:28:23 -0600

    Added createRef to ImageCard component for individual image sizing

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 
 class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.imageRef = createRef();
+  }
+
+  async componentDidMount() {
+    const image = await this.imageRef.current.clientHeight;
+    console.log(image);
+  }
+
   render() {
     const { urls, description } = this.props.image;
 
     return (
       <div>
-        <img alt={description} src={urls.regular} />
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
